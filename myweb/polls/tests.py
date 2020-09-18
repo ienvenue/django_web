95d8b3c62c9187d2dfd42ed31a7515fad2ec789a
ienvenue <chenyoungjie@163.com>
2020-09-18 15:12:52 +0800

    test

--- myweb/polls/tests.py
+++ myweb/polls/tests.py
@@ -35,3 +35,12 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_recent_question_test(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
